142e173fd2ed85d494d22650f2f59ddf5e027c77
Dawei Wang <daweiw@mun.ca>
2019-04-14 02:01:25 -0230

    Added batchnorm_forward in layers.py

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -177,7 +177,14 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # Referencing the original paper (https://arxiv.org/abs/1502.03167)   #
         # might prove to be helpful.                                          #
         #######################################################################
-        pass
+        mean = np.mean(x, axis=0)
+        var = np.var(x, axis=0)
+        
+        out = (x - mean)/np.sqrt(var+eps)
+        out = gamma * out + beta
+        
+        running_mean = momentum * running_mean + (1-momentum) * mean
+        running_var = momentum * running_var + (1-momentum) * var
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -188,7 +195,8 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # then scale and shift the normalized data using gamma and beta.      #
         # Store the result in the out variable.                               #
         #######################################################################
-        pass
+        out = (x - running_mean)/np.sqrt(running_var+eps)
+        out = gamma * out + beta
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
